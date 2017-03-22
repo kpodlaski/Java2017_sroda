6374e975f8a997bc7797a6580939ccc9cd93e1e2
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2017-03-22 16:45:52 +0100

    zadanie na mapy

---
+++ src/zajecia5/Car.java
@@ -0,0 +1,11 @@
+package zajecia5;
+
+/**
+ * Created by Krzysztof Podlaski on 22.03.2017.
+ */
+public class Car {
+    public int id;
+    public String name;
+    public String plateID;
+    public int milage;
+}
---
+++ src/zajecia5/Driver.java
@@ -0,0 +1,10 @@
+package zajecia5;
+
+/**
+ * Created by Krzysztof Podlaski on 22.03.2017.
+ */
+public class Driver {
+    public int id;
+    public String name;
+    public String licenceExpireDate; //Data ważności prawa jazdy
+}
